fd7c617c01cfec51fbb29eb0042e8f5ea4e91e6a
Chibunud <ndubest56@gmail.com>
2023-12-10 14:37:07 +0100

    format bookings well

--- src/pages/components/BookingsPage.jsx
+++ src/pages/components/BookingsPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import AccountNav from './AccountNav'
 import axios from 'axios'
 import { format } from 'date-fns'
+import { CalenderIcon, ThumbUpIcon } from '../../assets/icons/Logo'
 
 const BookingsPage = () => {
   const [bookings, setBookings] = useState([])
@@ -31,22 +32,23 @@ const BookingsPage = () => {
                 />
               </div>
               <div className='mt-3'>
-                <p className='text-xl font-semibold'>{booked.placeId.title}</p>
-                <p>
-                  from{' '}
+                <div className='text-xl font-semibold py-3 grow pr-3 flex gap-2'>
+                  <ThumbUpIcon />
+                  {booked.placeId.title}
+                </div>
+                <div className='flex gap-3'>
+                  <CalenderIcon />
+                  {booked.days} nights from{' '}
                   <span className='font-semibold'>
                     {format(new Date(booked.checkIn), `dd-MM-yyyy`)}
                   </span>
-                  {` `}to{` `}
+                  {` `} &rarr; {` `}
                   <span className='font-semibold'>
                     {format(new Date(booked.checkOut), `dd-MM-yyyy`)}
                   </span>
-                </p>
+                </div>
                 <div>
-                  Number of Guest: {booked.maxGuests} <br />
-                  Number of Nights: {booked.days}
-                  <br />
-                  Total amount: {booked.amount}
+                  Number of Guest: {booked.maxGuests} | Total amount: N{booked.amount}
                 </div>
               </div>
             </div>
