7bedc96c6f74da6c61e91ac5cb0515b17b3361f5
Chibunud <ndubest56@gmail.com>
2023-12-10 12:02:18 +0100

    all booking listing

--- src/pages/components/BookingsPage.jsx
+++ src/pages/components/BookingsPage.jsx
@@ -18,9 +18,25 @@ const BookingsPage = () => {
     <div>
       <AccountNav />
       <div>
-        {bookings.map((booked) => (
-          <div key={booked._id}>{booked.name}</div>
-        ))}
+        {bookings?.length > 0 &&
+          bookings.map((booked) => (
+            <div
+              key={booked._id}
+              className='flex gap-4 bg-gray-200 mt-10 rounded-2xl overflow-hidden '
+            >
+              <div className=' w-52'>
+                <img
+                  src={`${import.meta.env.VITE_API_URL}/uploads/${booked.placeId.photos?.[0]}`}
+                />
+              </div>
+              <div>
+                <p>{booked.placeId.title}</p>
+                <p>
+                  from {booked.checkIn} to {booked.checkOut}
+                </p>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   )
